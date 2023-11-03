6446fcc5beb00a1202063093ede7c42c625d7904
BDunnCode <BDunnCode@github.com>
2023-11-03 16:07:59 -0500

    lots of state and react specific changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
 import { useReducer } from "react"
+import DigitButton from "./DigitButton"
+import OperationButton from "./OperationButton"
 import "./styles.css"
 
-function reducer(state, action) {
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
 
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") 
+        return state
+      if (payload.digit === "." && state.currentOperand.includes(".")) 
+        return state
+
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+  }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer)
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+  
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">1234</div>
-        <div className="current-operand">5678</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
     <button className="span-two all-clear">AC</button>
     <button className="delete">DEL</button>
-    <button className="÷">÷</button>
-    <button className="1">1</button>
-    <button className="2">2</button>
-    <button className="3">3</button>
-    <button className="*">*</button>
-    <button className="4">4</button>
-    <button className="5">5</button>
-    <button className="6">6</button>
-    <button className="+">+</button>
-    <button className="7">7</button>
-    <button className="8">8</button>
-    <button className="9">9</button>
-    <button className="-">-</button>
-    <button className=".">.</button>
-    <button className="0">0</button>
+    <OperationButton operation="÷" dispatch={dispatch} />
+    <DigitButton digit="1" dispatch={dispatch} />
+    <DigitButton digit="2" dispatch={dispatch} />
+    <DigitButton digit="3" dispatch={dispatch} />
+    <OperationButton operation="*" dispatch={dispatch} />
+    <DigitButton digit="4" dispatch={dispatch} />
+    <DigitButton digit="5" dispatch={dispatch} />
+    <DigitButton digit="6" dispatch={dispatch} />
+    <OperationButton operation="+" dispatch={dispatch} />
+    <DigitButton digit="7" dispatch={dispatch} />
+    <DigitButton digit="8" dispatch={dispatch} />
+    <DigitButton digit="9" dispatch={dispatch} />
+    <OperationButton operation="-" dispatch={dispatch} />
+    <DigitButton digit="0" dispatch={dispatch} />
+    <DigitButton digit="." dispatch={dispatch} />
     <button className="span-two equal">=</button>
     </div>
   );
