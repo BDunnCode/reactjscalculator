7d8576a2d245f42c5b9de9b064fff0d1cae4ee67
BDunnCode <BDunnCode@github.com>
2023-11-03 12:02:48 -0500

    next commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import "./styles.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+    </div>
+    <button className="span-two all-clear">AC</button>
+    <button className="delete">DEL</button>
+    <button className="÷">÷</button>
+    <button className="1">1</button>
+    <button className="2">2</button>
+    <button className="3">3</button>
+    <button className="*">*</button>
+    <button className="4">4</button>
+    <button className="5">5</button>
+    <button className="6">6</button>
+    <button className="+">+</button>
+    <button className="7">7</button>
+    <button className="8">8</button>
+    <button className="9">9</button>
+    <button className="-">-</button>
+    <button className=".">.</button>
+    <button className="0">0</button>
+    <button className="span-two equal">=</button>
     </div>
   );
 }
